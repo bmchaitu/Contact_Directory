94fb2bf876003c366b34870c51217a1ed1a595aa
Chaitanya <budagavichaitu@gmail.com>
2025-04-02 15:48:39 +0530

    refactor event handler, add contact edit feature

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./Display.css";
 import PhoneContext from "./PhoneContext";
 
@@ -12,18 +12,56 @@ const Display = () => {
     handleAdd,
     handleDelete,
   } = useContext(PhoneContext);
+  const [isEdit, setisEdit] = useState(false);
+  const [contact, setContact] = useState({});
+
+  const onNameChange = event => {
+    handleName(event.target.value);
+  }
+
+  const onNumberChange = event => {
+    handleNumber(event.target.value);
+  }
+
+  // CREATE ONE MORE FUNCTION - onNumberChange
+  // PASS EVENT TO THAT FUNCTION
+  // CALL handleNumber with event.target.value
+  // PASS THAT NEW FUNCTION TO ONCHANGE in LINE 34
+
+
+  const onEdit = (givenIndex) => {
+    const foundContact = contacts.find((_, currentIndex) => currentIndex === givenIndex);
+    setContact(foundContact);
+    handleName(foundContact.name);
+    handleNumber(foundContact.number);
+    setisEdit(true);
+  }
+
+  const onAdd = () => {
+    handleAdd(isEdit, contact.id);
+    setisEdit(false);
+  }
 
   return (
     <>
       <div className="ContactsContainer">
         <div className="AddContacts">
           <label>Contact Name</label>
-          <input type="text" value={name} onChange={handleName} />
+          <input type="text" value={name} onChange={onNameChange} />
 
           <label>Contact Number</label>
-          <input type="text" value={number} onChange={handleNumber} />
+          <input type="text" value={number} onChange={onNumberChange} />
         </div>
-        <button className="AddButton" onClick={handleAdd}>
+        <input 
+          type='checkbox'
+          name="isEdit"
+          id="isEdit"
+          defaultValue={isEdit}
+          onChange={(event) => setisEdit(event.target.checked)}
+        />
+        <label htmlFor="isEdit">IS EDIT</label>
+        <br/>
+        <button className="AddButton" onClick={onAdd}>
           Add
         </button>
       </div>
@@ -39,8 +77,11 @@ const Display = () => {
             >
               Delete
             </button>
-            <br />
-            <button className="EditButton">Edit</button>
+            <button 
+              className="EditButton" 
+              onClick={() => onEdit(index)}>
+              Edit
+            </button>
           </div>
         ))}
       </div>
